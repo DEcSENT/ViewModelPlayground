1e216137bac773b141a1c40bac07c6e3a0ed6d1a
d.verentsov <d.verentsov@redmadrobot.com>
2018-09-06 18:15:19 +0300

    Initial commit.

---
+++ app/src/main/java/com/dvinc/viewmodelplayground/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dvinc.viewmodelplayground.presentation.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dvinc.viewmodelplayground.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
