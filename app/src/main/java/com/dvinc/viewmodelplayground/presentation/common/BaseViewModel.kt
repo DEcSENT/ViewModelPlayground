f2ec2c5de1450b3f2a1484f2dd5a3c507fa93ede
DEcSENT <decsent@yandex.ru>
2018-09-10 22:53:48 +0300

    Added Rx and updated BaseViewModel.

--- app/src/main/java/com/dvinc/viewmodelplayground/presentation/common/BaseViewModel.kt
+++ app/src/main/java/com/dvinc/viewmodelplayground/presentation/common/BaseViewModel.kt
@@ -1,13 +1,19 @@
 package com.dvinc.viewmodelplayground.presentation.common
 
 import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 abstract class BaseViewModel : ViewModel() {
 
-    //TODO: Add compositeDisp here
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCleared() {
         super.onCleared()
-        //TODO: clear subscs here
+        compositeDisposable.clear()
+    }
+
+    fun addSubscription(disposable: Disposable) {
+        compositeDisposable.add(disposable)
     }
 }
